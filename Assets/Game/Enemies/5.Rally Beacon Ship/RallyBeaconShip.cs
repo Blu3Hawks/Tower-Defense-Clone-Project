a7e3bea5a7f78a7312237e921c61c0bd27399bb3
Blu3Hawks <ylb.hen.sayag@gmail.com>
2024-05-19 17:29:25 +0300

    Rally Beacon
    First implementation of the Rally Beacon Ship enemy type

--- Assets/Game/Enemies/5.Rally Beacon Ship/RallyBeaconShip.cs
+++ Assets/Game/Enemies/5.Rally Beacon Ship/RallyBeaconShip.cs
@@ -5,4 +5,18 @@ using UnityEngine;
 public class RallyBeaconShip : Enemy // buffs nearby enemies with extra movespeed !
 {
     [SerializeField] private float buffRadius;
+    [SerializeField] private int movementSpeedBuffPercentage;
+    protected override void Update()
+    {
+        base.Update();
+        BuffingAllies();
+    }
+
+    private void BuffingAllies()
+    {
+        float appliedMoveSpeedBuff = (100 + movementSpeedBuffPercentage) / 100;
+
+        
+    }
+
 }
