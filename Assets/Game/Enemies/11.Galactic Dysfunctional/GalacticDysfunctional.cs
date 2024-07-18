2c72f7db811e2bceb59703a24b3b1f6733347070
Blu3Hawks <ylb.hen.sayag@gmail.com>
2024-07-18 12:29:57 +0300

    galactic dysfunctional finished
    this tower works !

--- Assets/Game/Enemies/11.Galactic Dysfunctional/GalacticDysfunctional.cs
+++ Assets/Game/Enemies/11.Galactic Dysfunctional/GalacticDysfunctional.cs
@@ -4,19 +4,151 @@ using UnityEngine;
 
 public class GalacticDysfunctional : Enemy //will be randomly generated between a few types - each type will reduce a value in nearby towers' attributes
 {
-    // Types to have - damage reduction, attack speed reduction, range reduction
     private enum DebuffType
     {
         DamageReduction,
         AttackSpeedReduction,
-        RangeReduction
+        RangeReduction,
     }
 
-    private DebuffType debuffType = new DebuffType();
+    // Types to have - damage reduction, attack speed reduction, range reduction
+    [Header("Values")]
+    [SerializeField] private float rangeOfEffect;
+    [SerializeField] private float debuffDuration; // for how long will the debuff last
+    [SerializeField] private float debuffTimer; // in how long will a debuff take action
+    [SerializeField] private float reductionPercentage;
+
+    private float calculatedValue; // will take the value we set and take the completing value of it to 1 - so if I put 30% off it means there's 70% on
+
+    private float currentDebuffTimer; // in how long will a debuff take action
+
+    private List<Tower> nearbyTowers = new List<Tower>();
+    private static System.Array debuffValues = System.Enum.GetValues(typeof(DebuffType));
+
+    protected override void Start()
+    {
+        base.Start();
+        RandomSetup();
+        currentDebuffTimer = debuffTimer;
+        calculatedValue = 1 - (reductionPercentage / 100f);
+    }
+
+    private DebuffType debuffType;
 
     private void RandomSetup()
     {
-        
+        debuffType = (DebuffType)debuffValues.GetValue(Random.Range(0, debuffValues.Length));
+        Debug.Log(this.debuffType.ToString());
     }
 
+    protected override void Update()
+    {
+        base.Update();
+        DebuffActivationTimer();
+    }
+
+    private void DebuffActivationTimer() // in how long will the debuff be activated
+    {
+        if (currentDebuffTimer > 0)
+        {
+            currentDebuffTimer -= Time.deltaTime;
+        }
+        if (currentDebuffTimer <= 0)
+        {
+            StartCoroutine(DebuffingNearbyTowers());
+            Debug.Log("We made a debuff to nearby towers!");
+            currentDebuffTimer = debuffTimer;
+        }
+    }
+
+    private IEnumerator DebuffingNearbyTowers()
+    {
+        LookForNearbyTowers();
+        foreach (Tower tower in nearbyTowers)
+        {
+            switch (debuffType)
+            {
+                case DebuffType.DamageReduction:
+                    DamageReductionDebuff(tower);
+                    break;
+                case DebuffType.AttackSpeedReduction:
+                    AttackSpeedReduction(tower);
+                    break;
+                case DebuffType.RangeReduction:
+                    AttackRangeReduction(tower);
+                    break;
+            }
+        }
+
+        yield return new WaitForSeconds(debuffDuration);
+
+        ResetValues();
+        nearbyTowers.Clear();
+    }
+
+    private void ResetValues()
+    {
+        foreach (Tower tower in nearbyTowers)
+        {
+            switch (debuffType)
+            {
+                case DebuffType.DamageReduction:
+                    DamageReductionReset(tower);
+                    break;
+                case DebuffType.AttackSpeedReduction:
+                    AttackSpeedReset(tower);
+                    break;
+                case DebuffType.RangeReduction:
+                    AttackRangeReset(tower);
+                    break;
+            }
+        }
+        Debug.Log("We just reset the values");
+    }
+
+    private void LookForNearbyTowers()
+    {
+        nearbyTowers.Clear();
+        Collider[] colliders = Physics.OverlapSphere(transform.position, rangeOfEffect);
+        foreach (Collider collider in colliders)
+        {
+            Tower tower = collider.GetComponent<Tower>();
+            if (tower != null)
+            {
+                nearbyTowers.Add(tower);
+            }
+        }
+    }
+
+    private void DamageReductionDebuff(Tower tower)
+    {
+        // Let's say the enemy does 3 damage.
+        // Let's say our reduced percentage is 30 - meaning 0.7 of the original damage - resulting in 2.1 damage.
+        tower.damageAmountPerParticle *= calculatedValue;
+    }
+
+    private void DamageReductionReset(Tower tower)
+    {
+        tower.damageAmountPerParticle /= calculatedValue;
+    }
+
+    private void AttackSpeedReduction(Tower tower)
+    {
+        tower.attackRate *= calculatedValue;
+    }
+
+    private void AttackSpeedReset(Tower tower)
+    {
+        tower.attackRate /= calculatedValue;
+    }
+
+    private void AttackRangeReduction(Tower tower)
+    {
+        tower.range *= calculatedValue;
+    }
+
+    private void AttackRangeReset(Tower tower)
+    {
+        tower.range /= calculatedValue;
+    }
 }
--- Assets/Game/Enemies/5.Rally Beacon Ship/RallyBeaconShip.cs
+++ Assets/Game/Enemies/5.Rally Beacon Ship/RallyBeaconShip.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,6 +7,7 @@ public class RallyBeaconShip : Enemy // buffs nearby enemies with extra movespee
 {
     [SerializeField] private float buffRadius;
     [SerializeField] private int movementSpeedBuffPercentage;
+    private List<Enemy> enemiesInRange = new List<Enemy>();
 
     protected override void Start()
     {
@@ -20,7 +20,7 @@ public class RallyBeaconShip : Enemy // buffs nearby enemies with extra movespee
         base.Update();
         ApplyingNearbyEnemiesBuff();
     }
-        
+
     private void ApplyingNearbyEnemiesBuff() // needs a change - this is buffing the movespeed every time ! needs to be only once !
     {
         float appliedMoveSpeedBuff = (100 + movementSpeedBuffPercentage) / 100;
@@ -32,14 +32,30 @@ public class RallyBeaconShip : Enemy // buffs nearby enemies with extra movespee
             if (enemy != null && !enemy.isMoveSpeedBuffed) // checking if it doesn't contain the enemy
             {
                 enemy.isMoveSpeedBuffed = true;
+                enemiesInRange.Add(enemy);
                 BuffEnemy(enemy, appliedMoveSpeedBuff);
             }
         }
-    }   
-    
+    }
+
     private void BuffEnemy(Enemy enemy, float speedBuff)
     {
         NavMeshAgent agent = enemy.navMeshAgent;
         agent.speed += speedBuff;
+    }
+
+    private void RemoveBuffs()
+    {
+        foreach (Enemy enemy in enemiesInRange)
+        {
+            float appliedMoveSpeedBuff = (100 + movementSpeedBuffPercentage) / 100;
+            NavMeshAgent agent = enemy.navMeshAgent;
+            agent.speed -= appliedMoveSpeedBuff;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        RemoveBuffs();
     }
 }
